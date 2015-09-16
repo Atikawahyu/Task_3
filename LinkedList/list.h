f61cce43af277327febf565de05fee0106e3cb7d
rizzauliarahman <rizzauliarahman@gmail.com>
2015-09-16 22:29:24 +0700

    list.h
    Rizza Aulia Rahman
    1301144231
    Yang dikerjakan :
    - Inisialisasi fungsi search
    - Inisialisasi prosedur menampilkan data customer

--- LinkedList/list.h
+++ LinkedList/list.h
@@ -1,6 +1,26 @@
 #ifndef LIST_H_INCLUDED
 #define LIST_H_INCLUDED
 
+struct infotype
+{
+    char no_hp[14], nama [20], alamat[100],email[50];
+    int ID_customer;
+};
 
+typedef struct elmlist *address;
+
+struct elmlist
+{
+    infotype info;
+    address next;
+};
+
+struct list
+{
+    address first;
+};
+
+address searchCustomer(list l, infotype x);
+void printInfo (list l);
 
 #endif // LIST_H_INCLUDED
